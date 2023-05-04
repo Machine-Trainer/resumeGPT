994d0370d15b18478cad0c9ad691f1d5adc64982
Machine-Trainer <weijianzeng2020@gmail.com>
2023-05-04 17:33:54 -0500

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="container" fluid >
+        <img src="src/assets/logo.png" class="logo"/>
+        <MainPage />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<style>
+.logo{
+  width: 10%;
+  height: auto;
+
+}
+.container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+<script setup lang="ts">
+  import MainPage from '@/components/MainPage.vue'
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        label="First name"
+        :rules="rules"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="lastName"
+        label="Last name"
+        :rules="rules"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Generate</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+<script>
+  export default {
+    data: () => ({
+      jobTitle: '',
+      rules: [
+        value => {
+          if (value) return true
+          return 'You must enter this field.'
+        },
+      ],
+    }),
+  }
+</script>
