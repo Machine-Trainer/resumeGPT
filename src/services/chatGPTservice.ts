6b367df937691032854878e644dd7c8b880b6aac
Machine-Trainer <weijianzeng2020@gmail.com>
2023-05-06 18:23:17 -0500

    test

--- src/services/chatGPTservice.ts
+++ src/services/chatGPTservice.ts
@@ -7,8 +7,9 @@ export default async function generate(
   endDate: string,
   description: string,
 ){
-  const OPENAI_API_KEY = process.env.VUE_APP_OPENAI_API_KEY;
-  console.log(OPENAI_API_KEY);
+  const OPENAI_API_KEY = "";
+  console.log("Line 1",process.env.MY_TEST_KEY);
+  console.log("Line 2",window.process.env.VUE_APP_MY_TEST_KEY);
   const endpoint = 'https://api.openai.com/v1/chat/completions';
   const prompt = `Could you rewrite following experience into resume experience?\n. I have worked at ${company} as ${jobTitle} between ${startDate} and ${endDate} at ${location}. ${description}`;
   const requestBody = {
